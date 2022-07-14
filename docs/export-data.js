08c5a7639c02b8d086fd5238c1e1e1815eca096a
Alan Norton <git@nonration.al>
2022-07-14 14:32:57 -0400

    automatic syncing

--- docs/export-data.js
+++ docs/export-data.js
@@ -1,21 +1,37 @@
-#!/usr/bin/env node
+#!/usr/bin/env node --experimental-fetch
 
-const fs = require('fs')
+const { writeFileSync } = require('fs')
 
-const data = fs.readFileSync("./index.csv").toString().split("\n").map(e => e.trim()).map(e => e.split(',').map(e => e.trim()));
+const TOKEN_ID_INDEX = 0
+const NAME_INDEX = 3
+const DESCRIPTION_INDEX = 4
+const IMAGE_URL_INDEX = 5
 
-const token_id_index = 0
-const name_index = 3
-const desc_index = 4
-const image_url_index = 5
+const writeDataFiles = (rows) => {
+  console.log(rows)
 
-for (var i = 1; i < data.length; i++) {
-  const datum = {
-    name: data[i][name_index],
-    description: data[i][desc_index],
-    image: data[i][image_url_index],
+  for (var i = 1; i < rows.length; i++) {
+    const datum = {
+      name: rows[i][NAME_INDEX],
+      description: rows[i][DESCRIPTION_INDEX],
+      image: rows[i][IMAGE_URL_INDEX],
+    }
+
+    const fileName = `data/${rows[i][TOKEN_ID_INDEX]}.json`
+
+    console.log(fileName)
+    writeFileSync(fileName, JSON.stringify(datum, null, 2) + '\n')
   }
-
-  fs.writeFileSync(`data/${data[i][token_id_index]}.json`, JSON.stringify(datum, null, 2) + '\n')
 }
 
+fetch('https://docs.google.com/spreadsheets/d/e/2PACX-1vQmJKwu96Fk3AlCa7heA4rsqjtlEv-UOkYyl8xdIX0_PqnmKmOlwzucUkoPUY7My4Y7v0-AY8gdjW-o/pub?gid=0&single=true&output=csv')
+  .then((response) => response.text())
+  .then((body) => {
+    const data = body
+      .toString()
+      .split('\n')
+      .map((e) => e.trim())
+      .map((e) => e.split(',').map((e) => e.trim()))
+
+    writeDataFiles(data)
+  })
